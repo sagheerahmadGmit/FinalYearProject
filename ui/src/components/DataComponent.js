d56f2adcf0d83f5f461d8a3c1f7305a22aedc711
sagheerahmadGmit <37177234+sagheerahmadGmit@users.noreply.github.com>
2021-01-19 17:33:57 +0000

    added a small abstract to dissertation

--- ui/src/components/DataComponent.js
+++ ui/src/components/DataComponent.js
@@ -1,4 +1,51 @@
 import React from 'react'
 import DataService from '../services/DataService'
 
-class
+class DataComponent extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            users:[]
+        }
+    }
+
+    componentDidMount(){
+        DataService.getData().then((response) => {
+            this.setState({ users: response.data})
+        });
+    }
+
+    render (){
+        return (
+            <div>
+                <h1 className = "text-center"> Data List</h1>
+                <table className = "table table-striped">
+                    <thead>
+                    <tr>
+
+                        <td> User Id</td>
+                        <td> User First Name</td>
+                    </tr>
+
+                    </thead>
+                    <tbody>
+                    {
+                        this.state.users.map(
+                            user =>
+                                <tr key = {user.id}>
+                                    <td> {user.id}</td>
+                                    <td> {user.name}</td>
+                                </tr>
+                        )
+                    }
+
+                    </tbody>
+                </table>
+
+            </div>
+
+        )
+    }
+}
+
+export default DataComponent
