a2ad0a5a53d063c5a797ae8b7c2262a8937e4799
Mateusz Pawlowski <g00361162@gmit.ie>
2021-01-21 13:45:36 +0000

    Axios Working

--- ui/src/services/DataService.js
+++ ui/src/services/DataService.js
@@ -2,20 +2,6 @@ import axios from 'axios'
 
 const DATA_REST_API = 'http://localhost:8085/getTest';
 
-module.exports = {
-    entry: '../app.js',
-    output: {
-        filename: 'bundle.js'
-    },
-    externals: ['axios'],
-    resolve: {
-        alias: {
-            'js-data-angular': '../dist/js-data-angular.js'
-        }
-    }
-};
-
-
 class DataService {
     getData()
     {
