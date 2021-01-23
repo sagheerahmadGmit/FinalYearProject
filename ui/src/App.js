7ed9343cc9a5513b0a5fb7200b617b81ae1c0269
sagheerahmadGmit <37177234+sagheerahmadGmit@users.noreply.github.com>
2021-01-23 17:43:11 +0000

    added router-dom navigation to frontend

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,13 +1,37 @@
+// function App() {
+//   return (
+//       <div className="App">
+//         <DataComponent />
+//       </div>
+//   );
+// }
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import React from 'react';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Error from './components/Error';
+import Navigation from './components/Navigation';
 import DataComponent from "./components/DataComponent";
 
-function App() {
-  return (
-      <div className="App">
-        <DataComponent />
-      </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <Navigation />
+                    <Switch>
+                        <Route path="/" component={Home} exact/>
+                        <Route path="/about" component={About}/>
+                        <Route path="/contact" component={Contact}/>
+                        <Route path="/dataTest" component={DataComponent}/>
+                        <Route component={Error}/>
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
