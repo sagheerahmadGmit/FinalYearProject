6eed3f5d984876e3aa1e9c638475afac9718ced4
sagheerahmadGmit <37177234+sagheerahmadGmit@users.noreply.github.com>
2021-01-28 23:16:35 +0000

    remane footer

--- ui/src/App.js
+++ ui/src/App.js
@@ -9,7 +9,7 @@ import Navigation from './components/NavBar/Navigation';
 import DataComponent from "./components/DataComponent";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import Footer from "./components/Footer/footer";
+import Footer from "./components/Footer/testFooter";
 
 
 class App extends Component {
